ac0375694a403adb9fdf67674bb0d5d1921e625a
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2021-02-18 21:22:21 +0100

    finished up/down

--- main.js
+++ main.js
@@ -7,14 +7,13 @@ client.setEncoding('utf8');
 
 var win;
 var dataInterval;
+
 var oldDown = 0
-var newDown = 0;
-var resultDown = 0;
 var oldUp = 0
-var newUp = 0;
-var resultUp = 0;
+
 var timeInterval = 4;
-var firstReading=true
+var firstReadingDown=true 
+var firstReadingUp=true 
 
 function createWindow () {
   win = new BrowserWindow({
@@ -91,9 +90,10 @@ ipcMain.handle('disconnect', (event, ...args) => {
 
 // ... do actions on behalf of the Renderer
 ipcMain.handle('startGraf', (event, ...args) => { 
-   firstReading=true 
+   firstReadingDown=true 
+   firstReadingUp=true 
    dataInterval=setInterval(function () {
-      client.write('display portstatistics portnum 3\r\n');  
+      client.write('display portstatistics portnum 1\r\n');  
   }, timeInterval*1000);
 })
 
@@ -107,31 +107,33 @@ client.on('data', function (data) {
   //var trans = data.toString().match(/tx_byte_ok \s\s+:\s(\d+)/g)
   var transm = data.toString().match(/(?<=tx_byte_ok \s\s+:\s)\d+/g)
   //console.log("transm", transm)
-  //var receiv = data.toString().match(/(?<=rx_byte_ok \s\s+:\s)\d+/g)
- // console.log("receiv",receiv)
-  
-  
-
   if (transm) {
       //newDown= Number(String(trans).match(/(\d+)/g))
       //[ '881213013905' ] Izvadi string i pretvoru u number
-      newDown=Number(transm[0]) 
+      var newDown=Number(transm[0]) 
       
-      if(firstReading){
-        oldDown=newDown
-        oldUp=newUp
-        result=0
-        firstReading=false  
-       
-      }else{
-        resultDown= (newDown- oldDown)/timeInterval;//BAJTA/s
-        resultDown = resultDown * 8         //bits/s
-        resultDown = resultDown / 1000      //kbits/s
-        oldDown= newDown
-      }
-      console.log("RESULTs kbit/s", resultDown);
-      win.webContents.send('resultVal', resultDown)
+      var resultDown=calculateBitRateDown(newDown)
+      
+      firstReadingDown=false
+      
+      win.webContents.send('resultValDown', resultDown)
   }
+
+  var receiv = data.toString().match(/(?<=rx_byte_ok \s\s+:\s)\d+/g)
+  if (receiv) {
+ 
+      var newUp=Number(receiv[0]) 
+      
+      var resultUp=calculateBitRateUp(newUp)
+
+      firstReadingUp=false
+
+      win.webContents.send('resultValUp',  resultUp)
+     
+  }
+ 
+  
+
 });
 
 //cliesnt timeout
@@ -149,15 +151,25 @@ client.on('close', function() {
 });
 
 
-function calculateBitRate(newBajt, oldBajt){  
-      if(firstReading){
-        oldBajt=newBajt
+function calculateBitRateDown(newBajt){  
+      if(firstReadingDown){
+        oldDown=newBajt
         result=0
-        firstReading=false  
-       
       }else{
-        result= (newBajt- oldBajt)/timeInterval;//BAJTA/s
-        oldBajt= newBajt
+        result= (newBajt- oldDown)/timeInterval;//BAJTA/s
+        oldDown= newBajt
+        result = result * 8         //bits/s
+        result = result / 1000      //kbits/s
+      }
+      return result
+}
+function calculateBitRateUp(newBajt){  
+      if(firstReadingUp){
+        oldUp=newBajt
+        result=0
+      }else{
+        result= (newBajt- oldUp)/timeInterval;//BAJTA/s
+        oldUp= newBajt
         result = result * 8         //bits/s
         result = result / 1000      //kbits/s
       }
